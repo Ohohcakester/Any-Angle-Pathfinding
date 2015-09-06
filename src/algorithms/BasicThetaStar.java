92bb50b121e9da39834bd2790efe394b2d255f91
Ohohcakester <ohohgit@gmail.com>
2015-09-06 19:38:05 +0800

    Give BasicThetaStar its own implementation of TryRelaxNeighbour

--- src/algorithms/BasicThetaStar.java
+++ src/algorithms/BasicThetaStar.java
@@ -22,6 +22,25 @@ public class BasicThetaStar extends AStarStaticMemory {
     }
 
     @Override
+    protected void tryRelaxNeighbour(int current, int currentX, int currentY, int x, int y) {
+        if (!graph.isValidCoordinate(x, y))
+            return;
+        
+        int destination = toOneDimIndex(x,y);
+        if (visited(destination))
+            return;
+        if (parent(current) != -1 && parent(current) == parent(destination)) // OPTIMISATION: [TI]
+            return; // Idea: don't bother trying to relax if parents are equal. using triangle inequality.
+        if (!graph.neighbourLineOfSight(currentX, currentY, x, y))
+            return;
+        
+        if (relax(current, destination, 0)) {
+            // If relaxation is done.
+            pq.decreaseKey(destination, distance(destination) + heuristic(x,y));
+        }
+    }
+
+    @Override
     protected boolean relax(int u, int v, float weightUV) {
         // return true iff relaxation is done.
         
@@ -36,7 +55,7 @@ public class BasicThetaStar extends AStarStaticMemory {
             }
             return false;
         } else {
-            float newWeight = distance(u) + weightUV;
+            float newWeight = distance(u) + physicalDistance(u, v);
             if (newWeight < distance(v)) {
                 setDistance(v, newWeight);
                 setParent(v, u);
