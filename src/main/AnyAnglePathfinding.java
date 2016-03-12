1a7a32b1661898a64b1cb9a7111bf61cf3a5ca63
Ohohcakester <ohohgit@gmail.com>
2016-03-12 22:58:23 +0800

    Clean up comments in AnyAnglePathfinding.java

--- src/main/AnyAnglePathfinding.java
+++ src/main/AnyAnglePathfinding.java
@@ -23,9 +23,10 @@ import algorithms.visibilitygraph.BFSVisibilityGraph;
  * Instructions: Look for the main method.
  * We can either run tests or trace the algorithm.
  * 
- * When tracing algorithms,
- * Choose the maze in loadMaze();
- * Choose the algorithm in setDefaultAlgoFunction();
+ * To see a visualisation of an algorithm,
+ * 1) Set choice = 0 in the first line of main();
+ * 2) Choose a maze in the first line of loadMaze();
+ * 3) Choose an algorithm in the first line of setDefaultAlgoFunction();
  * 
  * The tracing / experimentation functions are detailed in the traceAlgorithm() method.
  */
@@ -34,8 +35,8 @@ public class AnyAnglePathfinding {
     public static final String PATH_MAZEDATA = "mazedata/";
     private static AlgoFunction algoFunction; // The algorithm is stored in this function.
 
-    public static void main(String[] args) { // uncomment the one you need to use.
-        int choice = 0;
+    public static void main(String[] args) {
+        int choice = 0; // Choose an operation. 0: Visualisation.run() should be the default.
 
         switch(choice) {
             case 0:
@@ -71,9 +72,9 @@ public class AnyAnglePathfinding {
                 int sizeX = 20;               // x-axis size of grid
                 int sizeY = 20;               // y-axis size of grid
 
-                int sx = 10;                   // x-coordinate of start point
+                int sx = 10;                  // x-coordinate of start point
                 int sy = 13;                  // y-coordinate of start point
-                int ex = 6;                  // x-coordinate of goal point
+                int ex = 6;                   // x-coordinate of goal point
                 int ey = 8;                   // y-coordinate of goal point
                 return DefaultGenerator.generateUnseeded(sizeX, sizeY, unblockedRatio, sx, sy, ex, ey);
             }
@@ -81,11 +82,11 @@ public class AnyAnglePathfinding {
                 int unblockedRatio = 17;      // chance of spawning a cluster of blocked tiles is 1 in unblockedRatio.
                 int seed = 1667327427;        // seed for the random.
                 
-                int sizeX = 40;              // x-axis size of grid
-                int sizeY = 40;              // y-axis size of grid
-                int sx = 6;                  // x-coordinate of start point
-                int sy = 10;                 // y-coordinate of start point
-                int ex = 39;                 // x-coordinate of goal point
+                int sizeX = 40;               // x-axis size of grid
+                int sizeY = 40;               // y-axis size of grid
+                int sx = 6;                   // x-coordinate of start point
+                int sy = 10;                  // y-coordinate of start point
+                int ex = 39;                  // x-coordinate of goal point
                 int ey = 32;                  // y-coordinate of goal point
                 return DefaultGenerator.generateSeeded(seed, sizeX, sizeY, unblockedRatio, sx, sy, ex, ey);
             }
@@ -100,7 +101,7 @@ public class AnyAnglePathfinding {
             case 6 :
                 return DefaultGenerator.generateSeededOld(-159182402, 15, 15, 9, 1, 1, 13, 12); // anya previously gave incorrect path
             case 7 :
-                return GraphImporter.importGraphFromFile("maze14x11.txt", 0, 0, 10, 10); // Maze to contradict Theta* / A*
+                return GraphImporter.importGraphFromFile("maze14x11.txt", 0, 0, 10, 10); // Maze to contradict Theta* / A* optimality
             case 8 :
                 return GraphImporter.importGraphFromFile("mazeWCS.txt", 2, 0, 28, 25); // Worst Case Scenario path length.
             case 9 :
@@ -110,9 +111,9 @@ public class AnyAnglePathfinding {
             case 11 :
                 return GraphImporter.importGraphFromFile("mazeReuseWCS.txt", 1, 28, 0, 27); // Worst Case Scenario for Visibility Graph reuse.
             case 12 :
-                return GraphImporter.importGraphFromFile("anyaCont2.txt", 1, 6, 9, 1); // anya gives incorrect path
+                return GraphImporter.importGraphFromFile("anyaCont2.txt", 1, 6, 9, 1); // difficult case for anya
             case 13 :
-                return DefaultGenerator.generateSeededOld(-524446332, 20, 20, 10, 2, 19, 17, 2); // anya gives incorrect path.
+                return DefaultGenerator.generateSeededOld(-524446332, 20, 20, 10, 2, 19, 17, 2); // anya previously gave incorrect path
             case 14 :
                 return DefaultGenerator.generateSeededOld(-1155797147, 47, 32, 38, 46, 30, 20, 1); // issue for Strict Theta*
             case 15 :
@@ -154,7 +155,7 @@ public class AnyAnglePathfinding {
             case 32:
                 return GraphImporter.loadStoredMaze("corr2_maze512-2-1", "219-187_186-334");
             case 33 :
-                return GraphImporter.importGraphFromFile("anyaCont2b.txt", 9, 9, 9, 1); // anya gives incorrect path
+                return GraphImporter.importGraphFromFile("anyaCont2b.txt", 9, 9, 9, 1); // difficult case for anya
             default :
                 return null;
         }
@@ -211,7 +212,6 @@ public class AnyAnglePathfinding {
                 break;
             case 15 :
                 algoFunction = RecursiveStrictThetaStar::new;
-                //algoFunction = StrictThetaStarV2e::postSmooth;
                 break;
             case 16 :
                 algoFunction = BFSVisibilityGraph::graphReuse;
