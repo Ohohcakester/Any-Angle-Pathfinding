c7059a1bee2776f7cf91587cc165ac8c8a273452
Ohohcakester <ohohgit@gmail.com>
2016-03-27 07:20:01 +0800

    Add counters to TimeCounter

--- src/main/utility/TimeCounter.java
+++ src/main/utility/TimeCounter.java
@@ -14,6 +14,12 @@ public class TimeCounter {
     public static long timeC;
     public static long timeD;
     public static long timeE;
+
+    public static int counterA;
+    public static int counterB;
+    public static int counterC;
+    public static int counterD;
+    public static int counterE;
     
     public static int iterations;
     
@@ -23,6 +29,11 @@ public class TimeCounter {
     public static long timeC_freeze;
     public static long timeD_freeze;
     public static long timeE_freeze;
+    public static int counterA_freeze;
+    public static int counterB_freeze;
+    public static int counterC_freeze;
+    public static int counterD_freeze;
+    public static int counterE_freeze;
     public static int iterations_freeze;
     
     public static void reset() {
@@ -32,6 +43,11 @@ public class TimeCounter {
         timeC = 0;
         timeD = 0;
         timeE = 0;
+        counterA = 0;
+        counterB = 0;
+        counterC = 0;
+        counterD = 0;
+        counterE = 0;
         iterations = 0;
     }
     
@@ -45,6 +61,11 @@ public class TimeCounter {
         timeC_freeze = timeC;
         timeD_freeze = timeD;
         timeE_freeze = timeE;
+        counterA_freeze = counterA;
+        counterB_freeze = counterB;
+        counterC_freeze = counterC;
+        counterD_freeze = counterD;
+        counterE_freeze = counterE;
         iterations_freeze = iterations;
         
         isFrozen = true;
@@ -67,6 +88,11 @@ public class TimeCounter {
         timeC = timeC_freeze;
         timeD = timeD_freeze;
         timeE = timeE_freeze;
+        counterA = counterA_freeze;
+        counterB = counterB_freeze;
+        counterC = counterC_freeze;
+        counterD = counterD_freeze;
+        counterE = counterE_freeze;
         iterations = iterations_freeze;
     }
 
@@ -78,6 +104,10 @@ public class TimeCounter {
         return value / 1000000. / iterations;
     }
 
+    private static double per(int value) {
+        return value / iterations;
+    }
+
     public static void print() {
         tryRetrieveFrozenValues();
         
@@ -86,6 +116,11 @@ public class TimeCounter {
         System.out.println("Time C: " + toSecs(timeC));
         System.out.println("Time D: " + toSecs(timeD));
         System.out.println("Time E: " + toSecs(timeE));
+        if (counterA != 0) System.out.println("Count A: " + counterA);
+        if (counterB != 0) System.out.println("Count B: " + counterB);
+        if (counterC != 0) System.out.println("Count C: " + counterC);
+        if (counterD != 0) System.out.println("Count D: " + counterD);
+        if (counterE != 0) System.out.println("Count E: " + counterE);
 
         System.out.println("Iterations: " + iterations);
     }
@@ -98,6 +133,11 @@ public class TimeCounter {
         System.out.println("Time C: " + toSecsPer(timeC));
         System.out.println("Time D: " + toSecsPer(timeD));
         System.out.println("Time E: " + toSecsPer(timeE));
+        if (counterA != 0) System.out.println("Count A: " + per(counterA));
+        if (counterB != 0) System.out.println("Count B: " + per(counterB));
+        if (counterC != 0) System.out.println("Count C: " + per(counterC));
+        if (counterD != 0) System.out.println("Count D: " + per(counterD));
+        if (counterE != 0) System.out.println("Count E: " + per(counterE));
 
         System.out.println("Iterations: " + iterations);
     }
