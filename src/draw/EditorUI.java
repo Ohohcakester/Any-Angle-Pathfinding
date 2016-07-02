0ef5350416eeb8b3bc282e924e5559753e6fd3e4
Ohohcakester <ohohgit@gmail.com>
2016-07-02 18:08:18 +0800

    Add functionality to generate .map and .map.scen files from mazes and testcases.

--- src/draw/EditorUI.java
+++ src/draw/EditorUI.java
@@ -1,14 +1,18 @@
 package draw;
 
 import grid.GridGraph;
+import grid.StartGoalPoints;
 
 import java.awt.Color;
 import java.util.ArrayList;
 
+import main.AnyAnglePathfinding;
 import main.analysis.ProblemAnalysis;
 import main.analysis.TwoPoint;
 import main.testgen.Stringifier;
 import main.testgen.TestDataGenerator;
+import main.utility.Utility;
+import uiandio.FileIO;
 import algorithms.datatypes.Point;
 
 public class EditorUI extends DrawCanvas {
@@ -20,13 +24,25 @@ public class EditorUI extends DrawCanvas {
     private final ArrayList<ArrayList<Point>> connectedSets;
     private final String mazeName;
 
-    public EditorUI(GridGraph gridGraph, ArrayList<ArrayList<Point>> connectedSets, String mazeName) {
+    public EditorUI(GridGraph gridGraph, ArrayList<ArrayList<Point>> connectedSets, String mazeName, StartGoalPoints startGoalPoints) {
         super(gridGraph);
         this.mazeName = mazeName;
         this.connectedSets = connectedSets;
         pointSet = new GridPointSet();
         pointSet.setMinCircleSize();
-        sx = sy = ex = ey = -1;
+        
+        if (startGoalPoints == null) {
+            sx = -1;
+            sy = -1;
+            ex = -1;
+            ey = -1;
+        } else {
+            sx = startGoalPoints.sx;
+            sy = startGoalPoints.sy;
+            ex = startGoalPoints.ex;
+            ey = startGoalPoints.ey;
+        }
+        refreshPoints();
     }
     
     public void addStartPoint(int x, int y) {
@@ -92,6 +108,56 @@ public class EditorUI extends DrawCanvas {
         System.out.println(problemAnalysis);
         System.out.println("-Problem Name:---------------------");
         System.out.println(Stringifier.makeProblemName(sx, sy, ex, ey));
+    }
+
+    public void generateScen() {
+        // TODO Auto-generated method stub
+        String filePath = AnyAnglePathfinding.PATH_ANALYSISDATA;
+        String mapName = mazeName;
+        double shortestPath = Utility.computeOptimalPathLength(gridGraph, new Point(sx,sy), new Point(ex,ey));
+
+        System.out.println("-Writing to folder: " + filePath);
+        FileIO.makeDirs(filePath);
+        {
+            // Generate .map file
+            FileIO fileIO = new FileIO(filePath + mapName + ".map");
+
+            fileIO.writeLine("type octile");
+            fileIO.writeLine("height " + gridGraph.sizeY);
+            fileIO.writeLine("width " + gridGraph.sizeX);
+            fileIO.writeLine("map");
+            for (int y=0;y<gridGraph.sizeY;++y) {
+                StringBuilder sb = new StringBuilder();
+                for (int x=0;x<gridGraph.sizeX;++x) {
+                    sb.append(gridGraph.isBlocked(x,y) ? "@" : ".");
+                }
+                fileIO.writeLine(sb.toString());
+            }
+            fileIO.close();
+        }
+        {
+            // Generate .scen file
+            FileIO fileIO = new FileIO(filePath + mapName + ".map.scen");
+            
+            fileIO.writeLine("version 1");
+            String[] s = new String[]{
+                "1",
+                mapName + ".map",
+                gridGraph.sizeX + "",
+                gridGraph.sizeY + "",
+                sx + "",
+                sy + "",
+                ex + "",
+                ey + "",
+                shortestPath + ""
+            };
+            fileIO.writeLine(String.join("\t", s));
+            fileIO.close();
+        }
+        
+        System.out.println("-Write complete.");
+        
+        
     }
 
 }
